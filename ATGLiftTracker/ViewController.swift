581a245973db4ccbe8743a303e68abd58b8f1a84
Colin Morrison <colinjmmorris@gmail.com>
2022-12-09 18:42:13 +0000

    First Commit

---
+++ ATGLiftTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ATG Lift Tracker
+//
+//  Created by Colin Morrison on 09/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
